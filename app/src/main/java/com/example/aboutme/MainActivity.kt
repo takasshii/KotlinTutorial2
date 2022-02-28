96d6d5d738ce24493ace3bdaa19249af5b1bbc88
takasshii <takashiho0913@gmail.com>
2022-02-28 23:20:23 +0900

    section2-4 data-binding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,38 +1,45 @@
 package com.example.aboutme
 
 import android.content.Context
-import android.opengl.Visibility
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+
+import com.example.aboutme.databinding.ActivityMainBinding
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private  val myName: MyName = MyName("Aleks Haecky")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.myName = myName
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         //ボタンが押されたときに
-        findViewById<Button>(R.id.done_button).setOnClickListener{
+        binding.doneButton.setOnClickListener{
             //it→parentの親
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text
+        binding.apply {
+            //EditはEditableなので、String型に変更する必要がある。
+            myName?.nickname = nicknameEdit.text.toString()
+            //nicknameが変更されたら更新する
+            invalidateAll()
 
-        //入力したテキストを非表示に
-        editText.visibility = View.GONE
-        //ボタンを非表示に
-        view.visibility = View.GONE
-        //ニックネーム表示を表示する
-        nicknameTextView.visibility = View.VISIBLE
+            //入力したテキストを非表示に
+            binding.nicknameEdit.visibility = View.GONE
+            //ボタンを非表示に
+            binding.doneButton.visibility = View.GONE
+            //ニックネーム表示を表示する
+            binding.nicknameText.visibility = View.VISIBLE
+        }
+
 
         // Hide the keyboard.
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
